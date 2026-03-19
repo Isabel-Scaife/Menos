0e74eeb3b2ea13d84542ded20094eb0b7ee00957
EmmettS29007 <cas1899@g.rit.edu>
2026-03-18 23:04:04 -0400

    Major Guard Changes
    > Bird trigger began-- detects player in zone, but doesn't switch to bird
    > Hide boxes mostly done-- input is the issue here
    > Guard movement introduced, but needs to be made more modular. For now, they can go back and forth a preset amount.
    > Guard removed from main test thing so I don't explode anything
    > I really really hope this won't explode the repo--it shouldn't--but if it does I'm very sorry

---
+++ GreekGame/Assets/Scripts/BirdTrigger.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+/// <summary>
+/// Person that can be alerted by player's movements
+/// </summary>
+public class BirdTrigger : MonoBehaviour
+{
+
+    // fields
+    [SerializeField] //what does serialize feed do....
+    bool playerDetected = false;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private PlayerControlled player;
+    [SerializeField]
+    private Bird bird;
+
+    // run on start
+    void Start()
+    {
+        // Get the SpriteRenderer component attached to this GameObject
+        //we have this for debug purposes so it can change color
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    /// <summary>
+    /// When player enters trigger zone
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //for debug purposes changes it to red if player detected
+        spriteRenderer.color = Color.red;
+
+        //theoretically changes into a bird, but this doesn't quite work
+        player.SwapControlledObject();
+    }
+    
+}
