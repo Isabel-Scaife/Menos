a4abc30802658c0c2acb85c0879b26ac3b03ff21
EmmettS29007 <cas1899@g.rit.edu>
2026-03-19 23:08:50 -0400

    Guard Sprites Implemented
    > Dummy sprites for guard added!

--- GreekGame/Assets/Scenes/Scripts/Guard.cs
+++ GreekGame/Assets/Scenes/Scripts/Guard.cs
@@ -40,6 +40,14 @@ public class Guard : MonoBehaviour
     [SerializeField]
     private Bird bird;
 
+    //ping pong...
+    //needed for guard walking later on
+    float pingPong = 0;
+    float oldpingPong = 0;
+
+    //animation
+    private Animator animator;
+
     //GUARD TO DO:
 
     //HIGH PRIORITY-- Vision --  DONE
@@ -59,6 +67,8 @@ public class Guard : MonoBehaviour
         coneRenderer = visionCone.GetComponent<SpriteRenderer>();
         coneRenderer.enabled = true;
 
+        //get animating component
+        animator = GetComponent<Animator>();
 
         //figures out where the object is
         position = gameObject.transform.position;
@@ -70,11 +80,27 @@ public class Guard : MonoBehaviour
 
     void Update()
     {
+        oldpingPong = pingPong;
+        pingPong = Mathf.PingPong(Time.time * 2, max - min);
         //moves guard back and forth
         transform.position =
-            new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min,
+            new Vector3(pingPong + min,
             transform.position.y,
             transform.position.x);
+
+        //compares new ping pong value to old
+        //this tells us which direction the guard is walking
+        //and we can put the sprite in there accordingly
+        if (oldpingPong>pingPong)
+        {
+            //walking left
+            animator.Play("GuardWalkLeft");
+        }
+        else if (pingPong>oldpingPong)
+        {
+            //walking right
+            animator.Play("GuardWalkRight");
+        }
 
         VisionCheck(player);
     }
@@ -103,7 +129,7 @@ public class Guard : MonoBehaviour
     /// /param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        spriteRenderer.color = Color.red;
+        ;spriteRenderer.color = Color.red;
         //run respawn/fail state code here
     }
 
