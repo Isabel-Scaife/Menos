15d7f4d98baeec24bae3a3cef421714393973b80
bug-type-trainer <ajk5756@rit.edu>
2026-04-03 00:01:55 -0400

    jail guards disappear after talking to them and drop key

--- GreekGame/Assets/Scripts/Interactables/NPCGuard.cs
+++ GreekGame/Assets/Scripts/Interactables/NPCGuard.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
 
-public class NPCGuard : MonoBehaviour
+/// <summary>
+/// Guard that walks down after being interacted with
+/// </summary>
+public class NPCGuard : NPC
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Interactable key;
+    [SerializeField]
+    private NPCGuard otherGuard;
+    public bool HasKey { get; set; }
+
+    // needs other guard to give this one the key
+    private void Start()
     {
-        
+        HasKey = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Shows dialogue
+    /// </summary>
+    /// <param name="player">player interacting with this NPC</param>
+    public override void Interact(PlayerControlled player)
     {
-        
+        // exits early if no interaction is allowed at this time
+        if (!canInteract) return;
+
+        // shows dialogue, passes key to other guard if they're still there, then destroys self
+        canInteract = false;
+        if (dialogues != null && dialogues.Count > 0)
+        {
+            if (DialogueManager.Instance == null) Debug.Log("No DialogueManager in scene");
+            else DialogueManager.Instance.BeginDialogue(dialogues[0], player);
+        }
+        if (otherGuard)
+        {
+            otherGuard.HasKey = true;
+        }
+        else
+        {
+            key.transform.SetPositionAndRotation(this.transform.position, Quaternion.identity);
+            key.gameObject.SetActive(true);
+        }
+        Destroy(this.gameObject);
     }
 }
