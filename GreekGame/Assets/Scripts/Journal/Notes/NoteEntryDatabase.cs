fb34d44a54bfb4f7619b5caf46e3dd68db3cc89e
Amy Lee <iamseoyoung05@gmail.com>
2026-04-02 22:48:36 -0400

    Journal Update

--- GreekGame/Assets/Scripts/Journal/Notes/NoteEntryDatabase.cs
+++ GreekGame/Assets/Scripts/Journal/Notes/NoteEntryDatabase.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "NoteEntryDatabase", menuName = "Scriptable Objects/NoteEntryDatabase")]
 public class NoteEntryDatabase : ScriptableObject
 {
-    public NoteEntryData[] noteEntries;
+    public string[] noteEntries;
 }
--- GreekGame/Assets/Scripts/Journal/Notes/NotesTabController.cs
+++ GreekGame/Assets/Scripts/Journal/Notes/NotesTabController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class NotesTabController : MonoBehaviour
 {
@@ -11,12 +12,16 @@ public class NotesTabController : MonoBehaviour
     [Header("Placeholder for Notes Display")]
     public TMP_Text[] slots;
 
-    public Image inputPopup;
+    // Holder for where the temporary stuff will be saved
+    [SerializeField]public List<string> runTimeTexts;
 
-    public TMP_Text currentInput;
+    public Image inputPopup;
 
     public int notesPerPage = 18;
     public int currentPage = 0;
+
+    public delegate void SaveNote();
+    public static event SaveNote Save;
 
     private void Awake()
     {
@@ -32,7 +37,8 @@ public class NotesTabController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        runTimeTexts = new List<string>();
+        RefreshPage();
     }
 
     // Update is called once per frame
@@ -46,9 +52,30 @@ public class NotesTabController : MonoBehaviour
         Debug.Log("Write your stuff here!");
 
         inputPopup.gameObject.SetActive(true);
+
+        JournalManager.Instance.DisableJournalUIInput();
     }
 
-    // Progres page left (Updates the note page with 18 entries on left)
+    // Refresh Page (Shows 18 content from the note entry section that was saved depending on the current page)
+    public void RefreshPage()
+    {
+        // Starting index needs to be changed later when we progress through page
+        // It will be used to retrieve data from 'runTimeTexts'
+        int spacing = currentPage * notesPerPage;
+
+        for (int i = 0; i < notesPerPage; i++)
+        {
+            int index = i + spacing;
+
+            if(index < runTimeTexts.Count)
+            {
+                slots[i].text = runTimeTexts[i + spacing];
+            }
+        }
+    }
+
+
+    // Progress page left (Updates the note page with 18 entries on left)
 
     // Progress page right (Updates the note page with 18 entries on right)
 }
