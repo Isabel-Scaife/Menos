cfc8ed4aa639fa20d2a3cdce11f6861b8e34f9ef
Isabel Scaife <ims3502@rit.edu>
2026-06-11 18:12:59 -0400

    removed scenes

--- GreekGame/Assets/Scripts/Interactables/SavePoint.cs
+++ GreekGame/Assets/Scripts/Interactables/SavePoint.cs
@@ -28,7 +28,7 @@ public class SavePoint : Interactable
                 SaveSystem.SaveData<SpawnManager, SpawnManagerData>
                     (SpawnManager.Instance, "/SpawnManager.json");
             }
-
+               
             Debug.Log("Save Successful! Files found at: " + Application.persistentDataPath);
         }
         catch (Exception e)
