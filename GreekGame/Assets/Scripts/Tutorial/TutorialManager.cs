726e77711800c74ca0ebf1b6bf84bc767d536275
bug-type-trainer <ajk5756@rit.edu>
2026-06-14 04:59:58 -0400

    Wrote tutorial logic, still debugging

---
+++ GreekGame/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using TMPro;
+
+public class TutorialManager : MonoBehaviour
+{
+    // singleton
+    public static TutorialManager Instance { get; private set; }
+    
+    // fields
+    [SerializeField]
+    private RectTransform popupRect;
+
+    [SerializeField]
+    private TMP_Text popupTMP;
+
+    private TutorialTask currentTask;
+
+    private void Awake()
+    {
+        Instance = this;
+        currentTask = null;
+    }
+
+    private void Update()
+    {
+        // hides popup when task is completed        
+        if (currentTask != null && currentTask.Completed())
+        {
+            popupRect.gameObject.SetActive(false);
+            popupTMP.text = "";
+            currentTask = null;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        // update popup position on the screen after any camera movement
+        if (currentTask == null) return;
+        popupRect.position = Camera.main.WorldToScreenPoint(currentTask.anchor.position);
+    }
+
+    public void ShowTask(TutorialTask task)
+    {
+        // do not show if already showing a task
+        if (currentTask != null) return;
+        
+        currentTask = task;
+        popupTMP.text = currentTask.text;
+        popupRect.position = Camera.main.WorldToScreenPoint(currentTask.anchor.position);
+        popupRect.gameObject.SetActive(true);
+    }
+}
