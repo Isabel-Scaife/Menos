1b80ef8f7a2c05f1f4796cc837f75be69c2687c4
EmmettS29007 <cas1899@g.rit.edu>
2026-06-16 17:46:37 -0400

    Teleporting Now Works!
    > Teleporting (Using doors) now functions.
    > Prefab named "Teleport" has been added to prefabs folder
    > Teleports to blacksmith and post office have been added but they just teleport to nowhere
    > Teleports function by pairing to another door via serialized fields and will port the player and camera there
    > only concerns are the fact that camera smoothly moves instead of fully teleporting? No gameplay issues but im wondering if thats what we want
    > naming conventions for teleport doors are specific--can be found in commented script

--- GreekGame/Assets/Scripts/Interactables/teleportMechanic.cs
+++ GreekGame/Assets/Scripts/Interactables/teleportMechanic.cs
@@ -1,36 +1,45 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class doorMechanic : MonoBehaviour
+public class teleportMechanic : Interactable
 {
     [SerializeField]
     Player player;
+
     [SerializeField]
     GameObject camera;
 
-    // Teleportingggg
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //the other teleport door you want to spit the player out at
+    [SerializeField]
+    teleportMechanic teleportTo;
+
+    //coordinates to paired door
+    private Vector3 teleportToPos;
+
+    //detects if player is in front of the door
+    private bool inRange;
+
+    //naming convention: Teleport_CurrentLocation_WhereIt'sGoing
+
     void Start()
     {
-        
+        //gets coordinates to teleport to
+        teleportToPos = teleportTo.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// when player interacts with door
+    /// </summary>
+    /// <param name="player"></param>
+    public override void Interact(PlayerControlled player)
     {
-        //player.transform.position = new Vector3(500, 500, 0);
-    }
+        if (!canInteract) return;
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        Debug.Log("arf");
-        if (collision.gameObject.name == "Player")
-        {
-            //detects olive clicks and set it to dragging
-            if (Input.GetMouseButton(0))
-            {
-                camera.transform.position = new Vector3(100, 100, 0);
-                player.transform.position = new Vector3(100, 100, 0);
-            }
-        }
+        //teleports both player and camera
+         //must teleport both, otherwise camera goes all wonky and stops rendering
+         //subtracts y so it teleports you slightly in front of the door
+        camera.transform.position = new Vector3(teleportToPos.x, teleportToPos.y - 5, 0);
+        player.transform.position = new Vector3(teleportToPos.x, teleportToPos.y - 5, 0);
+
     }
 }
