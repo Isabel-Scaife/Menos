6306964f31acc575455fd27444559bf4833de6ea
Isabel Scaife <ims3502@rit.edu>
2026-06-23 12:53:38 -0400

    added fade to black when teleporting via door

---
+++ GreekGame/Assets/Scripts/Interactables/MapTeleport.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+
+public class MapTeleport : Interactable
+{
+    private enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        NoOffset
+    }
+
+    [Header("Camera")]
+    [SerializeField] Transform cameraPos;
+    [SerializeField] PolygonCollider2D mapBounds;
+
+    private CinemachineConfiner2D confiner;
+
+    [Header("Teloport Location")]
+    [SerializeField] Transform teleportTo;
+    [SerializeField] Direction direction;
+    [SerializeField] float offset = 5;
+
+    private Vector3 newPos;
+
+    private void Awake()
+    {
+        confiner = Object.FindAnyObjectByType<CinemachineConfiner2D>();
+
+        // determine teleport position 
+        newPos = teleportTo.position;
+
+        switch (direction)
+        {
+            case Direction.Up: newPos.y += offset; break;
+            case Direction.Down: newPos.y -= offset; break;
+            case Direction.Left: newPos.x -= offset; break;
+            case Direction.Right: newPos.x += offset; break;
+        }
+    }
+
+    /// <summary>
+    /// fade to black, teleport, fade back in 
+    /// </summary>
+    async void FadeTransition(PlayerControlled player)
+    {
+        await ScreenFader.Instance.FadeOut();
+
+        // apply new camera bounds
+        confiner.BoundingShape2D = mapBounds;
+
+        // set position
+        //cameraPos.transform.position = newPos;
+        player.transform.position = newPos;
+
+        await ScreenFader.Instance.FadeIn();
+    }
+
+    /// <summary>
+    /// when player interacts with object teleport player
+    /// </summary>
+    /// <param name="player"></param>
+    public override void Interact(PlayerControlled player)
+    {
+        if (!canInteract) return;
+
+        FadeTransition(player);
+
+    }
+}
---
+++ GreekGame/Assets/Scripts/UI/ScreenFader.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Threading.Tasks;
+using Unity.Cinemachine;
+
+public class ScreenFader : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private float fadeDuration = 0.5f;
+    [SerializeField] private CinemachinePositionComposer cinemachinePositionComposer;
+    
+    private Vector3 orginalDamping;
+
+    public static ScreenFader Instance { get; private set; }
+    void Awake()
+    {
+        if (Instance!= null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        orginalDamping = cinemachinePositionComposer.Damping;
+    }
+
+    private async Task Fade(float targetTransparency)
+    {
+        float start = canvasGroup.alpha; 
+        float t = 0;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(start, targetTransparency, t / fadeDuration);
+            await Task.Yield();
+        }
+        canvasGroup.alpha = targetTransparency;
+    }
+
+    /// <summary>
+    /// Fade to black
+    /// </summary>
+    public async Task FadeOut()
+    {
+        await Fade(1);
+        SetDamping(Vector3.zero);
+    }
+
+    /// <summary>
+    /// Fade to transparent
+    /// </summary>
+    public async Task FadeIn()
+    {
+        await Fade(0);
+        SetDamping(orginalDamping);
+    }
+
+    /// <summary>
+    /// Set camera damping 
+    /// </summary>
+    public void SetDamping(Vector3 damping)
+    {
+        cinemachinePositionComposer.Damping = damping; 
+    }
+}
