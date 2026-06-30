6e65521c5d1f8c925f73d2687d85ca22f819b377
Isabel Scaife <ims3502@rit.edu>
2026-06-30 15:51:12 -0400

    jail cutscene progress
    - after cutscene ends seamlessly switch to dialogue
    - working on disabling player movement /visuals (waiting for update from ayvin)

---
+++ GreekGame/Assets/Scripts/CutScene/ScenePart.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ScenePart : MonoBehaviour
+{
+    [Header("Scene Part to Play")]
+    [SerializeField] private PlayableDirector thisCutscene;
+    [SerializeField] private DialogueSO thisDialogue;
+
+    [Header("After Scene Finishes")]
+    [SerializeField] private GameObject nextScene;
+    [SerializeField] private Player player;
+    [SerializeField] private GameObject disableAfter;
+
+    private void Awake()
+    {
+        // play cut scene if avaiable 
+        if (thisCutscene != null)
+        {
+            thisCutscene.Play();
+        }
+
+        // play dialogue if avaiable 
+        if (thisDialogue != null)
+        {
+            if (DialogueManager.Instance == null) { Debug.Log("No DialogueManager in scene"); return; }
+            //player.gameObject.SetActive(true);      // temp fix just so i can accept dialogue input
+            DialogueManager.Instance.BeginDialogue(thisDialogue, player);
+        }
+    }
+
+    private void Update()
+    {
+        // check if scene completed 
+        if (thisCutscene != null && thisCutscene.state == PlayState.Paused ||
+            thisDialogue != null && !DialogueManager.Instance.DialogueIsHappening)
+        {
+            // play next scene 
+            if (nextScene != null)
+            {
+                nextScene.SetActive(true);
+                this.gameObject.SetActive(false);
+                return;
+            }
+
+            // no next scene return to player
+            player.gameObject.SetActive(true);
+            disableAfter.SetActive(false);
+        }
+
+    }
+
+}
