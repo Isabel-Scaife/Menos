d01e942e8d265f2b6916110fcf817aa74427535e
Isabel Scaife <ims3502@rit.edu>
2026-08-05 01:40:39 -0400

    added evidence field to vase minigame complete

--- GreekGame/Assets/Scripts/Interactables/CutSceneVase.cs
+++ GreekGame/Assets/Scripts/Interactables/CutSceneVase.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class CutSceneVase : Vase
 {
+    [SerializeField] private EvidenceData evidence;
     [SerializeField] private GameObject cutSceneParent;
 
     protected override void HandleComplete()
     {
         base.HandleComplete();
 
+        JournalManager.Instance.UnlockEvidence(evidence);
         cutSceneParent.SetActive(true);
     }
 }
