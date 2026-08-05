3f91c8aba4698360bb174c39ce1de098e443b948
Isabel Scaife <ims3502@rit.edu>
2026-08-04 22:42:44 -0400

    added cutscene after vase

---
+++ GreekGame/Assets/Scripts/Interactables/CutSceneVase.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CutSceneVase : Vase
+{
+    [SerializeField] private GameObject cutSceneParent;
+
+    protected override void HandleComplete()
+    {
+        base.HandleComplete();
+
+        cutSceneParent.SetActive(true);
+    }
+}
