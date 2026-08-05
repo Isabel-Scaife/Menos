dc18553080dfac0c5656ebde6f6145707c6a84dd
Isabel Scaife <ims3502@rit.edu>
2026-08-05 02:13:46 -0400

    added on trigger enter cutscene script

---
+++ GreekGame/Assets/Scripts/CutScene/TriggerCutscene.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class TriggerCutscene : MonoBehaviour
+{
+    [SerializeField] private GameObject cutsceneParent;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        cutsceneParent.SetActive(true);
+    }
+}
